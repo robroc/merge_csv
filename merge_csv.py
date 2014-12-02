8981d37d7ced049398c3fdde6989224b7d4fa46f
Roberto Rocha <robroc1@gmail.com>
2014-12-02 17:13:53 -0500

    Updated script

--- merge_csv.py
+++ merge_csv.py
@@ -1,22 +1,26 @@
-
 # Script to merge several CSVs into one
 
 import glob
+import csv
+
 csvs = glob.glob('*.csv')
 
 f = open('merged_data.csv', 'wb')
 master_csv = csv.writer(f)
 
 # Take the header of the first CSV and make it the master header
-current_csv = open(csvs[0], 'rb')
-headers = current_csv.readline().strip().split(';')
+first_csv = open(csvs[0], 'rb')
+headers = first_csv.readline().strip().split(';')
 master_csv.writerow(headers)
 # Write remaining rows
-for line in current_csv:
-    master_csv.writerow(line.strip())
+for line in first_csv:
+    master_csv.writerow(line.strip().split(';'))
 
-# Read reminaing CSVs andskip the first row
-for csv in csvs[1:]:
-    for line_num, line in enumerate(csv):
+# Read remaining CSVs and skip the first row
+for file in csvs[1:]:
+    current_csv = open(file, 'rb')
+    for line_num, line in enumerate(current_csv):
         if line_num > 0:
             master_csv.writerow(line.strip().split(';'))
+            
+f.close()
