6d8803b0dd80837d3cd30c657bdfda77451ce0db
Roberto Rocha <robroc1@gmail.com>
2014-12-03 10:56:45 -0500

    Made it easier to replace delimiter

--- merge_csv.py
+++ merge_csv.py
@@ -3,6 +3,9 @@
 import glob
 import csv
 
+# Change delimiter to the appropriate one in CSV
+delimiter = ';'
+
 csvs = glob.glob('*.csv')
 
 f = open('merged_data.csv', 'wb')
@@ -10,17 +13,17 @@ master_csv = csv.writer(f)
 
 # Take the header of the first CSV and make it the master header
 first_csv = open(csvs[0], 'rb')
-headers = first_csv.readline().strip().split(';')
+headers = first_csv.readline().strip().split(delimiter)
 master_csv.writerow(headers)
 # Write remaining rows
 for line in first_csv:
-    master_csv.writerow(line.strip().split(';'))
+    master_csv.writerow(line.strip().split(delimiter))
 
 # Read remaining CSVs and skip the first row
 for file in csvs[1:]:
     current_csv = open(file, 'rb')
     for line_num, line in enumerate(current_csv):
         if line_num > 0:
-            master_csv.writerow(line.strip().split(';'))
+            master_csv.writerow(line.strip().split(delimiter))
             
 f.close()
